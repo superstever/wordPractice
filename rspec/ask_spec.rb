9bbe390864509d23c87821b4334a34587cbb2a1a
Stephen Lee <superstever@gmail.com>
2011-04-09 12:50:14 -0400

    added new methods that will suggest the correct index given a direction of the first case. Also makes a suggestion to the user.

--- rspec/ask_spec.rb
+++ rspec/ask_spec.rb
@@ -34,23 +34,23 @@ class Ask
     end
     
     it "gets a 'high' in any case and formats it to a 1" do
-      directionResponse('h').should equal(1)
-      directionResponse('H').should equal(1)
-      directionResponse('high').should equal(1)
-      directionResponse('High').should equal(1)
-      directionResponse('hi').should equal(1)  
-      directionResponse('Hi').should equal(1) 
-      directionResponse('hiGH').should equal(1)     
+      directionResponse('h').should equal(-1)
+      directionResponse('H').should equal(-1)
+      directionResponse('high').should equal(-1)
+      directionResponse('High').should equal(-1)
+      directionResponse('hi').should equal(-1)  
+      directionResponse('Hi').should equal(-1) 
+      directionResponse('hiGH').should equal(-1)     
     end
     
     it "gets a 'low' in any case and formats it to a -1" do
-      directionResponse('l').should equal(-1)
-      directionResponse('L').should equal(-1) 
-      directionResponse('low').should equal(-1) 
-      directionResponse('Low').should equal(-1)
-      directionResponse('lo').should equal(-1)  
-      directionResponse('Lo').should equal(-1) 
-      directionResponse('lOW').should equal(-1) 
+      directionResponse('l').should equal(1)
+      directionResponse('L').should equal(1) 
+      directionResponse('low').should equal(1) 
+      directionResponse('Low').should equal(1)
+      directionResponse('lo').should equal(1)  
+      directionResponse('Lo').should equal(1) 
+      directionResponse('lOW').should equal(1) 
     end
     
     it "gets a 'done' in any case and formats it to a 0" do
@@ -75,33 +75,71 @@ class Ask
     #midQt = 20 - 12 + 1 = 9/2 = 4.5 floor = 4
     #we should have a lower range of 12 to 15; minIndex, minIndex+midQt-1
     #we should have an upper range of 16 to 20; minIndex+midQt, maxIndex
-    it "- should spit out the correct indices for an EVEN array with low direction" do
-      solnArray = [12, 16]
-      recalibrate(12, 21, 'low').should == solnArray
-    end
-    
-    it "- should spit out the correct indices for an ODD array with low direction" do
-      solnArray = [12, 15]
-      recalibrate(12, 20, 'low').should == solnArray
-    end
-    
     it "- should spit out the correct indices for an EVEN array with high direction" do
-      solnArray = [17, 21]
-      recalibrate(12, 21, 'high').should == solnArray
+      solnArray = [12, 16]
+      giveHalf(12, 21, 'high').should == solnArray
     end
     
     it "- should spit out the correct indices for an ODD array with high direction" do
+      solnArray = [12, 15]
+      giveHalf(12, 20, 'high').should == solnArray
+    end
+    
+    it "- should spit out the correct indices for an EVEN array with low direction" do
+      solnArray = [17, 21]
+      giveHalf(12, 21, 'low').should == solnArray
+    end
+    
+    it "- should spit out the correct indices for an ODD array with low direction" do
       solnArray = [16, 20]
-      recalibrate(12, 20, 'high').should == solnArray
+      giveHalf(12, 20, 'low').should == solnArray
     end   
+
+    #maybe create a condition for done
+    
+    it "- should suggest the correct index given a direction on the first case" do
+      #If we give it an array of indices = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14
+      #and run midPt - we get a starting index to test. This is our first suggestion.
+      exampleIndices = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14
+      exampleArray = ["ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety", "one-hundred", "one-ten", "one-twenty", "one-thirty", "one-forty", "one-fifty"]
+      exampleIndices.length.should equal(exampleArray.length) #make sure my indices and exampleArray are the same length for the test cases
+      exampleArray[midQt(exampleArray.length)].should == ("eighty")
+    end
+    
+    it "- should then suggest the correct range to run recursion when given a low on the first case" do
+      #If we give it an array of indices = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14
+      #and run midPt - we get a starting index to test. This is our first suggestion.
+      exampleIndices = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14
+      exampleArray = ["ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety", "one-hundred", "one-ten", "one-twenty", "one-thirty", "one-forty", "one-fifty"]
+      userResponse = 'low'
+      solnArray = [1, 14]
+      updateWordRange(exampleArray.find_index('ten'), exampleArray.find_index('one-fifty'), 'low').should == solnArray
+    end
+    
+    it "- should then suggest the correct range to run recursion when given a high on the first case" do
+      exampleIndices = 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14
+      exampleArray = ["ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety", "one-hundred", "one-ten", "one-twenty", "one-thirty", "one-forty", "one-fifty"]
+      userResponse = 'high'
+      solnArray = [0, 13]
+      updateWordRange(exampleArray.find_index('ten'), exampleArray.find_index('one-fifty'), 'high').should == solnArray
+    end
+    
+    it "should make a suggestion for the user that is the max array end point if 'high'" do
+      exampleArray = ["ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety", "one-hundred", "one-ten", "one-twenty", "one-thirty", "one-forty", "one-fifty"]
+      suggest(exampleArray.find_index('twenty'), exampleArray.find_index('fifty'), "high").should  equal(exampleArray.find_index('forty'))
+    end
+    
+    it "should make a suggestion for the user that is the min array end point if 'low" do
+      exampleArray = ["ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety", "one-hundred", "one-ten", "one-twenty", "one-thirty", "one-forty", "one-fifty"]
+      suggest(exampleArray.find_index('twenty'), exampleArray.find_index('fifty'), "low").should  equal(exampleArray.find_index('thirty'))
+    end
   end #end "logic"
-  
-  describe "correct halves" do
-    it "gives the proper half-value when told 'lower'"
-    it "gives the proper half-value when told 'higher'"
-  end
-  
-  describe "finishing" do
-    it "ends gracefully when told 'done'"
-  end
-end
+
+#this finishing function causes the program to quit before it can get a reading. Explore stubs and mocks  http://stackoverflow.com/questions/1480537/how-can-i-validate-exits-and-aborts-in-rspec
+# describe "finishing" do
+#   it "ends gracefully when told 'done' in the 'giveHalf' method" do
+#   lambda giveHalf(1, 4, 'done').should exit_with_code(0)
+##   lambda giveHalf(1, 4, 'done').should raise_error SystemExit
+#  end
+# end
+end #end Class Ask
--- wordSandwich.rb
+++ wordSandwich.rb
@@ -21,9 +21,9 @@ end
 def directionResponse(userInput)
   case userInput.downcase
   when 'h', 'high', 'hi' then
-    return 1
-  when 'l','low', 'lo' then
     return -1
+  when 'l','low', 'lo' then
+    return 1
   when 'd', 'done', 'do', 'don' then
     return 0
   else 
@@ -47,18 +47,48 @@ end
 #midQt = 20 - 12 + 1 = 9/2 = 4.5 floor = 4
 #we should have a lower range of 12 to 15; minIndex, minIndex+midQt-1
 #we should have an upper range of 16 to 20; minIndex+midQt, maxIndex
-def recalibrate(wordArrayMin, wordArrayMax, directionInput)
+def giveHalf(wordArrayMin, wordArrayMax, directionInput)
   case directionResponse(directionInput)
   when -1 then
     offsetMidQ = midQt(wordArrayMax-wordArrayMin+1)
     newMid = wordArrayMin + offsetMidQ - 1
     return wordArrayMin, newMid
   when 0 then
-    return 'done0', 'done1'
+    quitProgram
   when 1 then
     offsetMidQ = midQt(wordArrayMax-wordArrayMin+1)
     newMid = wordArrayMin + offsetMidQ
     return newMid, wordArrayMax
+  end 
+end
+
+#when given an array range, discerned by the wordArrayMin and wordArrayMax, it will use the directionInput
+#to exclude the incorrectly suggested word in the new array range. For example, if we have an array
+#exampleArray = ["ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety", "one-hundred", "one-ten", "one-twenty", "one-thirty", "one-forty", "one-fifty"]
+#The user types in "ninety" in the Word Sandwich game. That value is too high. Then 
+def updateWordRange(wordArrayMin, wordArrayMax, directionInput)
+  case directionResponse(directionInput)
+  when -1 then
+    return wordArrayMin, wordArrayMax-1 #gives a new range that excludes the erroneous suggestion
+  when 1 then
+    return wordArrayMin+1, wordArrayMax
+  when 0 then
+    quitProgram
   end
-  
+end
+
+def suggest(wordArrayMin, wordArrayMax, directionInput)
+  case directionResponse(directionInput)
+  when -1 then
+    return wordArrayMax-1 #gives a new range that excludes the erroneous suggestion
+  when 1 then
+    return wordArrayMin+1
+  when 0 then
+    return 'done'
+  end
+end
+
+def quitProgram
+  Kernel.at_exit {puts "You clicked done. Exiting program!"}
+  Kernel.exit(status=true)
 end
